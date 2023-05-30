2c30e7bac07e4911cb5c7d629de956bc984c54d5
dixit-crest <dixit.p@crestinfosystems.net>
2023-05-30 19:50:29 +0530

    resoling setState meethod type error

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -3,11 +3,15 @@ import SetButton from "./SetButton";
 
 const Counter = () => {
   const [count, setCount] = useState<number>(0);
-  const handleIncrement = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    setCount(count => count + 1)
+  const handleIncrement = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setCount((count) => count + 1);
   };
-  const handleDecrement = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    setCount(count => count - 1)
+  const handleDecrement = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setCount((count) => count - 1);
   };
 
   return (
@@ -15,7 +19,7 @@ const Counter = () => {
       <button onClick={handleIncrement}>+</button>
       {count}
       <button onClick={handleDecrement}>-</button>
-    <SetButton value={5} setValue={setCount} />
+      <SetButton value={5} setValue={setCount} />
     </div>
   );
 };
